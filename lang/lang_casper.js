3b3375c5878000af4b044acc0f5e8380390162f9
utayo <t11131tu@sfc.keio.ac.jp>
2016-05-19 13:01:05 +0900

    ほげ
    .

--- lang/lang_casper.js
+++ lang/lang_casper.js
@@ -5,33 +5,26 @@ casper.start('http://vu.sfc.keio.ac.jp/course_u/data/2016/csec14_3.html',functio
 	this.echo("Language Home Window");
 	//	くっそ重いので作らない
 	//this.capture('lang_home_window.png');
-	/*
-	arr = this.evaluate(function(){
-		return Array.prototype.forEach.call(
-			document.querySelectorAll('div.course_title a'),
-			function(node){
-				console.log(node.href);
-				node.href;
-		});
-	});
-	this.echo(arr);
-	*/
-	//this.evaluate(function(){
+	
 		arr = this.evaluate(function(){
+			//	Selectorで持ってきた<a>要素のhrefを抜き出す
 			al = Array.prototype.slice.call(document.querySelectorAll('.course_title > a'));
 			return  al.map(function(a){
 				return a.getAttribute('href');
 			});
 		});
 		require('utils').dump(arr);
-	//});
-	//require('utils').dump(arr);
 });
 
-
+//	まずは一つ取ってこれるように
 casper.then(function(){
-	casper.start(arr[0], function(){
-		this.capture('asdf.png');
+	var i = 0;
+	arr.forEach(function(a){
+		casper.start(a, function(){
+			this.capture(i+'.png');
+		});
+		i++;
 	});
 });
+
 casper.run();
