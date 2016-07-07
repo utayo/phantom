fa34b65c3dcc5c303ae53bdf1e5f10bd9573e81e
utayo <t11131tu@sfc.keio.ac.jp>
2016-07-07 11:25:15 +0900

    idとtitleをrdf形式で出力
    他もやります

--- lang/get_html.js
+++ lang/get_html.js
@@ -9,13 +9,28 @@ casper.start(
 	'https://vu.sfc.keio.ac.jp/course2014/summary/syll_view_c.cgi?yc=2016_25037&ks=B2001', 
 	function(){
 		data = this.evaluate(function(){
-			arr = Array.prototype.slice.call(document.querySelectorAll('table'));
-			return arr.map(function(a){
-				return a.innerHTML;
-			});
+			var obj = {};	//	結果出力用
+			//	title, lecture_id
+			arr = Array.prototype.slice.call(document.querySelectorAll('.ctitle14'));
+			a = arr[0];
+			obj["lecture_id"] = a.querySelector('.sm').innerText;
+			a.querySelector('.sm').remove();
+			a.querySelector('br').remove();
+			obj["ja_name"] = a.innerText;
+			
+			//	table要素から取得したい
+
+			return obj;	//	返却
 		});
-		require('utils').dump(data);
-		fs.write('table.txt', data, 'a');
+		//console.log(data);
+		//require('utils').dump(data);
+		var rdf = "<http://example.com/lecture#1>\n";
+		rdf += '\tlecb:name "' + data["ja_name"] + '"@ja;\n';
+		rdf += '\tlecb:lecture_id "' + data["lecture_id"] + '";\n';
+		console.log(rdf);
+		fs.write('title.txt', rdf, 'a');
+		
+		
 });
 
 casper.run();
