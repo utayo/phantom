1670025b80deea9bd75e0f4c6923f22b8269636b
utayo <t11131tu@sfc.keio.ac.jp>
2016-09-29 00:21:42 +0900

    シラバスのページを見に行って、そこから帰ってこれるように
    evaluate内でclick()、waitを挟んでthenを仕込むと移動できそう。どれぐらいwaitするかはよくわからん。

--- keio/get_keio_syllabus.js
+++ keio/get_keio_syllabus.js
@@ -5,21 +5,21 @@ var casper = require('casper').create({
 var fs = require('fs');
 
 var index = 'https://gslbs.adst.keio.ac.jp/menu/Slt_Kamoku.php';
+var trr = "";
+
 
 casper.on('get_syllabus_link', function(addr){
     casper.then(function(){
-    var addr_list = Array.prototype.slice.call(document.querySelectorAll('ListTbl_in tr'));
-    this.echo(addr_list);
-         casper.capture("hoge.ong");
+        var addr_list = Array.prototype.slice.call(document.querySelectorAll('ListTbl_in tr'));
+        this.echo(addr_list);
+        casper.capture("hoge.png");
     });
-    
-
 });
 
 casper.start(index,
     function(){
         //var mita = document.querySelector("#chk_campus0");
-
+        
         this.evaluate(function(){
             document.querySelector('#chk_campus0').checked = true;
             document.form1.submit();
@@ -28,36 +28,41 @@ casper.start(index,
 );
 
 casper.then(function(){
-    /*
-    var tr_list = this.evaluate(function(){
-        //スペルちげえでやんの
-        var tr = document.querySelectorAll('.ListTbl_in tbody tr a');
-        return tr[1];
-    });
-
-    this.echo(tr_list.innerText);
-    */
+    this.echo(this.getHTML());
     this.echo(this.getCurrentUrl());
     //this.onclick('.ListTbl_in tr td:nth-of-type(5)');
     //this.evaluate(function(){
-    this.evaluate(function(){
+    trr = this.evaluate(function(){
+        var tr = Array.prototype.slice.call(document.querySelectorAll('.ListTbl_in tbody tr a'));
+        
+        /*
         document.form1.action = "https://gslbs.adst.keio.ac.jp/lecture/View_Lecture.php";
         document.form1.hid_lessoncd.value		 = '16505';
         document.form1.hid_tourokubango.value	 = '19367';
         document.form1.hid_nowpage.value = 0;
         document.form1.submit();
+        */
+        tr[1].click();
+        return tr[1].onclick;
     });
-        
-    //});
-    
-    //casper.capture("mita.png");
-    //casper.emit('get_syllabus_link', index);
+}).wait(10).then(function(){
+    this.capture('hogea.png');
+    this.evaluate(function(){
+        document.querySelector('.back a').click();
+    });
+    //back.click();
+}).wait(1000).then(function(){
+    this.echo('2nd');
+    this.capture('menu.png');
 });
 
+
+
 casper.then(function(){
+    this.echo("ho-ho"+trr);
     this.echo("hogw");
     this.echo(this.getCurrentUrl());
-    casper.capture("mita.png");
+    casper.capture("aaaa.png");
 });
 
 casper.run();
